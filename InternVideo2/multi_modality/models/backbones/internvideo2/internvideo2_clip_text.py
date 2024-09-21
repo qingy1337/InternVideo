8dae7f32dae156c0c8a35776ca627f175cf0874f
ruo2019 <64449301+ruo2019@users.noreply.github.com>
2024-09-21 11:25:26 -0700

    Update internvideo2_clip_text.py

--- InternVideo2/multi_modality/models/backbones/internvideo2/internvideo2_clip_text.py
+++ InternVideo2/multi_modality/models/backbones/internvideo2/internvideo2_clip_text.py
@@ -2,7 +2,6 @@ import logging
 import numpy as np
 import torch
 import torch.nn.functional as F
-import peft
 from peft import get_peft_model, LoraConfig, TaskType
 from torch import nn
 from transformers import LlamaForCausalLM, LlamaConfig
