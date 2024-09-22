5704acb98ec406c1e92f31d26fedcd0a8879d14d
qingy1337 <qingy2019@outlook.com>
2024-09-22 16:22:33 -0700

    Update utils.py

--- InternVideo2/multi_modality/demo/utils.py
+++ InternVideo2/multi_modality/demo/utils.py
@@ -136,7 +136,7 @@ class InternVideo2_Stage2(nn.Module):
         self.is_pretrain = is_pretrain
         self.vision_width = config.model.vision_encoder.clip_embed_dim
         print(config.model.text_encoder)
-        self.text_width = config.model.text_encoder.embed_dim
+        self.text_width = 512
         self.embed_dim = config.model.embed_dim
 
         # create modules.
